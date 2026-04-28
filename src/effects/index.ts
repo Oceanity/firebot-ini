3ba6356bbfa3585d23c7d95b05c68dec6d8b4796
Oceanity <github@oceanity.net>
2026-04-27 18:52:14 -0700

    Fixed EffectType Ids (oops, sorry folks who use this)

--- src/effects/index.ts
+++ src/effects/index.ts
@@ -1,3 +1,8 @@
+import { INI_INTEGRATION_ID } from "../constants";
 import { WriteIniValueEffectType } from "./write-ini-value";
 
-export const AllIniEffectTypes = [WriteIniValueEffectType];
+export const AllIniEffectTypes = [WriteIniValueEffectType].map((effectType) => {
+  effectType.definition.id = `${INI_INTEGRATION_ID}:${effectType.definition.id}`;
+
+  return effectType;
+});
