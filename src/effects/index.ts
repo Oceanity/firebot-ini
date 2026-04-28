2cb2eb3503911b11bec2a140975c5a677f5b6823
Oceanity <github@oceanity.net>
2026-04-27 19:39:39 -0700

    Actually eh let's leave it for now

--- src/effects/index.ts
+++ src/effects/index.ts
@@ -1,8 +1,3 @@
-import { INI_INTEGRATION_ID } from "../constants";
 import { WriteIniValueEffectType } from "./write-ini-value";
 
-export const AllIniEffectTypes = [WriteIniValueEffectType].map((effectType) => {
-  effectType.definition.id = `${INI_INTEGRATION_ID}:${effectType.definition.id}`;
-
-  return effectType;
-});
+export const AllIniEffectTypes = [WriteIniValueEffectType];
